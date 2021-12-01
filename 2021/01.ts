0bfb73827e92282a517c74ddfb4d8e7ed881b58f
Marko Mlinaric <marko.mlinaric@sclable.com>
2021-12-01 10:27:50 +0100

    solve 01

---
+++ 2021/01.ts
@@ -0,0 +1,21 @@
+import { readInput } from './utils/readInput'
+
+const INPUT = readInput('2021/01.input').map((i) => parseInt(i))
+
+let count1 = 0
+for (let i = 1; i < INPUT.length; i++) {
+  count1 += INPUT[i] > INPUT[i - 1] ? 1 : 0
+}
+
+console.log('Part 1', count1)
+
+const windows = new Array(INPUT.length - 2).fill(0).map(
+  (_, i) => INPUT[i] + INPUT[i + 1] + INPUT[i + 2]
+)
+
+let count2 = 0
+for (let i = 1; i < windows.length; i++) {
+  count2 += windows[i] > windows[i - 1] ? 1 : 0
+}
+
+console.log('Part 2', count2)
